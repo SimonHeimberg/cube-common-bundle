9d0852064fb5d9161d786c85694070052b0e1d93
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2017-02-28 12:23:33 +0100

    [cleanup] configuration with better namespace, gitignore with LF line end

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('emch_berger_cube_common');
+        $rootNode = $treeBuilder->root('cubetools_cube_common');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
