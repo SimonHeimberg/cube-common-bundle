eaa18734ff56fc5da7b93dae4e807885dc15c747
Krzysztof Ruszczynski <ruszczak@o2.pl>
2017-08-04 14:14:20 +0200

    XML extraction tool improved - reading of attributes, translations, change of source elements; additional tests added

--- src/DataHandling/XMLExtractor.php
+++ src/DataHandling/XMLExtractor.php
@@ -21,6 +21,11 @@ class XMLExtractor
     protected $transformations;
     
     /**
+     * @var array key is name of XML source element, value - name of column in output 
+     */
+    protected $translations;
+    
+    /**
      * @var array data read from XML (each array element is one row: in each row key is element name and value is read from xml)  
      */
     protected $readData;
@@ -29,11 +34,13 @@ class XMLExtractor
      * Constructor for XML extractor.
      * @param string $xpath path for xpath where subsequent elements are placed
      * @param array $transformations transformations (key is used to get element with the same name on $xpath level and place it under this key in output array)
+     * @param array $translations key is name of XML source element, value - name of column in output 
      */
-    public function __construct($xpath, $transformations)
+    public function __construct($xpath, $transformations, $translations=array())
     {
         $this->xpath = $xpath;
         $this->transformations = $transformations;
+        $this->translations = $translations;
     }
     
     /**
@@ -47,6 +54,48 @@ class XMLExtractor
         return $this;
     }
     
+    /**
+     * Example:
+     *      transformation has element 'country'
+     *      method is executed with ('location', 'country')
+     *      in that case we will be looking for element 'location' in XML and output it as 'country'
+     *      in XML we will not be looking for 'country' element anymore
+     * Convenient to use if we want dynamicaly change source of data without changing further code.  
+     * @param string $sourceElementName name of element in XML source
+     * @param string $outputElementName name of element in output from XML extraction (can replace current source element)
+     * @return \Ebbe\TestManager\EvaluationBundle\Logic\XMLExtractor object, on which this method was executed
+     */
+    public function addOrReplaceSourceElement($sourceElementName, $outputElementName=null)
+    {
+        if (is_null($outputElementName)) {
+            $outputElementName = $sourceElementName;
+        }
+        
+        if (isset($this->transformations[$outputElementName])) {
+            unset($this->transformations[$outputElementName]);
+        }
+        
+        $this->transformations[$sourceElementName] = true;  // value is not important
+        $this->addSourceTranslation($sourceElementName, $outputElementName);
+        return $this;
+    }
+    
+    /**
+     * Method for adding translations.
+     * @param string $sourceElementName name of element in XML source
+     * @param string $outputElementName name of element in output from XML extraction
+     * @return \Ebbe\TestManager\EvaluationBundle\Logic\XMLExtractor object, on which this method was executed
+     */
+    public function addSourceTranslation($sourceElementName, $outputElementName)
+    {
+        $this->translations[$sourceElementName] = $outputElementName;
+        return $this;
+    }
+    
+    /**
+     * Method extracting data from XML
+     * @return array extracted data from XML
+     */
     public function readSource()
     {
         $this->readData = array();
@@ -55,7 +104,29 @@ class XMLExtractor
             $readDataElement = array();
             
             foreach ($this->transformations as $elementName => $arrayValue) {
-                $readDataElement[$elementName] = $domElement->getElementsByTagName($elementName)->item(0)->nodeValue;
+                if(stripos($elementName, '/@') !== false){
+                    $elementArray = explode('/@', $elementName);
+                    $elementNodeName = $elementArray[0];
+                    $elementAttribute = $elementArray[1];
+                                        
+                    if (empty($elementNodeName)) { // getting attribute from actual element
+                        $elementDom = $domElement;
+                    } else {
+                        $elementDom = $domElement->getElementsByTagName($elementNodeName)->item(0);
+                    }
+                    
+                    $elementValue = $elementDom->getAttribute($elementAttribute);                    
+                } else {
+                    $elementValue = $domElement->getElementsByTagName($elementName)->item(0)->nodeValue;
+                }      
+                
+                if (isset($this->translations[$elementName])) { // make translation
+                    $elementKey = $this->translations[$elementName];
+                } else {
+                    $elementKey = $elementName;
+                }
+                
+                $readDataElement[$elementKey] = $elementValue;
             }
             
             $this->readData[] = $readDataElement;
--- tests/DataHandling/XMLExtractorTest.php
+++ tests/DataHandling/XMLExtractorTest.php
@@ -5,43 +5,150 @@ use CubeTools\CubeCommonBundle\DataHandling\XMLExtractor;
 
 class XMLExtractorTest extends \PHPUnit\Framework\TestCase
 {
+    /* data for testProcessXMLSimple */
+    
     /**
      * @var string XML source
      */
-    const REQUIREMENT_SOURCE = '<?xml version="1.0" encoding="UTF-8"?>
+    const SIMPLE_SOURCE = '<?xml version="1.0" encoding="UTF-8"?>
         <elements>
           <element>
-            <attribute1>at1</attribute1>
-            <attribute2>at2</attribute2>
-            <attribute3>at3</attribute3>
-            <attribute4>at4</attribute4>
+            <element1>el1</element1>
+            <element2>el2</element2>
+            <element3>el3</element3>
+            <element4>el4</element4>
           </element>
           <element>
-            <attribute1>at11</attribute1>
-            <attribute2>at22</attribute2>
-            <attribute3>at33</attribute3>
-            <attribute4>at44</attribute4>
+            <element1>el11</element1>
+            <element2>el22</element2>
+            <element3>el33</element3>
+            <element4>el44</element4>
           </element>  
         </elements>';
     
-    protected $transformations = array('attribute1' => '',
-        'attribute2' => '',
-        'attribute3' => '',
-        'attribute4' => ''
+    protected $simpleTransformations = array('element1' => '',
+        'element2' => '',
+        'element3' => '',
+        'element4' => '',
         );
     
-    protected $expectedReadArray = array(array(
-        'attribute1' => 'at1',
-        'attribute2' => 'at2',
-        'attribute3' => 'at3',
-        'attribute4' => 'at4'
+    protected $simpleExpectedReadArray = array(array(
+        'element1' => 'el1',
+        'element2' => 'el2',
+        'element3' => 'el3',
+        'element4' => 'el4',
     ),
     array(
-        'attribute1' => 'at11',
-        'attribute2' => 'at22',
-        'attribute3' => 'at33',
-        'attribute4' => 'at44'
+        'element1' => 'el11',
+        'element2' => 'el22',
+        'element3' => 'el33',
+        'element4' => 'el44',
     ));
+    
+    /* end of data for testProcessXMLSimple */
+    /* data for testProcessXMLWithAttributeInFirstElement */
+    
+    /**
+     * @var string XML source with element
+     */
+    const ATTRIBUTE_FIRST_ELEMENT_SOURCE = '<?xml version="1.0" encoding="UTF-8"?>
+        <elements>
+          <element attribute1="at1" attribute2="at2">
+            <element1>el1</element1>
+            <element2>el2</element2>
+            <element3>el3</element3>
+            <element4>el4</element4>
+          </element>
+          <element attribute1="at3" attribute2="at4">
+            <element1>el11</element1>
+            <element2>el22</element2>
+            <element3>el33</element3>
+            <element4>el44</element4>
+          </element>  
+        </elements>';
+    
+    protected $attributeFirstElementTransformations = array('/@attribute1' => '',
+        '/@attribute2' => '',
+        );
+    
+    protected $attributeFirstElementExpectedReadArray = array(array(
+        '/@attribute1' => 'at1',
+        '/@attribute2' => 'at2',
+    ),
+    array(
+        '/@attribute1' => 'at3',
+        '/@attribute2' => 'at4',
+    ));
+    
+    /* end of data for testProcessXMLWithAttributeInFirstElement */
+    
+    /* data for testProcessXMLWithAttributeInChild */
+    
+    /**
+     * @var string XML source with element
+     */
+    const ATTRIBUTE_IN_CHILD = '<?xml version="1.0" encoding="UTF-8"?>
+        <elements>
+          <element>
+            <element1 attribute1="at1">el1</element1>
+            <element2 attribute1="at2">el2</element2>
+            <element3>el3</element3>
+            <element4>el4</element4>
+          </element>
+          <element>
+            <element1 attribute1="at3">el11</element1>
+            <element2 attribute1="at4">el22</element2>
+            <element3>el33</element3>
+            <element4>el44</element4>
+          </element>  
+        </elements>';
+    
+    protected $attributeInChildTransformations = array('element1/@attribute1' => '',
+        'element2/@attribute1' => '',
+        );
+    
+    protected $attributeInChildExpectedReadArray = array(array(
+        'element1/@attribute1' => 'at1',
+        'element2/@attribute1' => 'at2',
+    ),
+    array(
+        'element1/@attribute1' => 'at3',
+        'element2/@attribute1' => 'at4',
+    ));
+    
+    /* end of data for testProcessXMLWithAttributeInChild */
+    /* data for testProcessXMLSimpleTranslation */
+    
+    protected $simpleTranslationExpectedReadArray = array(array(
+        'element_first' => 'el1',
+        'element2' => 'el2',
+        'element3' => 'el3',
+        'element4' => 'el4',
+    ),
+    array(
+        'element_first' => 'el11',
+        'element2' => 'el22',
+        'element3' => 'el33',
+        'element4' => 'el44',
+    ));
+    
+    /* end of data for testProcessXMLSimpleTranslation */
+    /* data for testProcessXMLSimpleReplaceSourceElement */
+    
+    protected $replaceSourceElementExpectedReadArray = array(array(
+        'dynamicElementName' => 'el1',
+        'element2' => 'el2',
+        'element3' => 'el3',
+        'element4' => 'el4',
+    ),
+    array(
+        'dynamicElementName' => 'el11',
+        'element2' => 'el22',
+        'element3' => 'el33',
+        'element4' => 'el44',
+    ));
+    
+    /* end of data for testProcessXMLSimpleReplaceSourceElement */
     
     protected $xpath = '//elements/element';
     
@@ -56,8 +163,7 @@ class XMLExtractorTest extends \PHPUnit\Framework\TestCase
      */
     protected function setUp()
     {
-        $this->object = new XMLExtractor($this->xpath, $this->transformations);
-        $this->object->setSource(self::REQUIREMENT_SOURCE);
+        
     }
 
     /**
@@ -69,9 +175,45 @@ class XMLExtractorTest extends \PHPUnit\Framework\TestCase
         
     }
     
-    public function testProcessXMLWithoutNamespaces()
+    public function testProcessXMLSimple()
     {
+        $this->object = new XMLExtractor($this->xpath, $this->simpleTransformations);
+        $this->object->setSource(self::SIMPLE_SOURCE);
         $readArray = $this->object->readSource();
-        $this->assertEquals($this->expectedReadArray, $readArray, 'Reading of XML source was not correct!');
+        $this->assertEquals($this->simpleExpectedReadArray, $readArray, 'Reading of XML source was not correct!');
+    }
+    
+    public function testProcessXMLWithAttributeInFirstElement()
+    {
+        $this->object = new XMLExtractor($this->xpath, $this->attributeFirstElementTransformations);
+        $this->object->setSource(self::ATTRIBUTE_FIRST_ELEMENT_SOURCE);
+        $readArray = $this->object->readSource();
+        $this->assertEquals($this->attributeFirstElementExpectedReadArray, $readArray, 'Reading of XML source was not correct!');
+    }
+    
+    public function testProcessXMLWithAttributeInChild()
+    {
+        $this->object = new XMLExtractor($this->xpath, $this->attributeInChildTransformations);
+        $this->object->setSource(self::ATTRIBUTE_IN_CHILD);
+        $readArray = $this->object->readSource();
+        $this->assertEquals($this->attributeInChildExpectedReadArray, $readArray, 'Reading of XML source was not correct!');
+    }
+    
+    public function testProcessXMLSimpleTranslation()
+    {
+        $this->object = new XMLExtractor($this->xpath, $this->simpleTransformations);
+        $this->object->addSourceTranslation('element1', 'element_first');
+        $this->object->setSource(self::SIMPLE_SOURCE);
+        $readArray = $this->object->readSource();
+        $this->assertEquals($this->simpleTranslationExpectedReadArray, $readArray, 'Reading of XML source was not correct!');
+    }
+    
+    public function testProcessXMLSimpleReplaceSourceElement()
+    {
+        $this->object = new XMLExtractor($this->xpath, $this->simpleTransformations);
+        $this->object->addOrReplaceSourceElement('element1', 'dynamicElementName');
+        $this->object->setSource(self::SIMPLE_SOURCE);
+        $readArray = $this->object->readSource();
+        $this->assertEquals($this->replaceSourceElementExpectedReadArray, $readArray, 'Reading of XML source was not correct!');
     }
 }
